9bc9a9fdbdc56439a89061983927279db929c654
mdrazaulkabir <razaulkabirit@gmail.com>
2026-04-13 23:59:42 +0600

    fist digit even or odd added another problem

--- week2/fist_digit_even_odd.c
+++ week2/fist_digit_even_odd.c
@@ -1,8 +1,25 @@
+// #include<stdio.h>
+// int main(){
+//     ///this problem quesetion given number will be 4 digit contian fist digit =>4343(even or odd)
+//     int a;
+//     scanf("%d",&a);
+//     int d=a/1000;
+//     if(d%2==0){
+//         printf("EVEN");
+//     }
+//     else{
+//         printf("ODD");
+//     }
+//     return 0;
+// }
+
+
 #include<stdio.h>
 int main(){
+    ///this problem quesetion given number will be 4 digit contian last digit =>4343(even or odd)
     int a;
     scanf("%d",&a);
-    int d=a/1000;
+    int d=a%10;
     if(d%2==0){
         printf("EVEN");
     }
