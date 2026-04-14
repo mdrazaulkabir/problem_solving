f68dcb31adfb19452d8b66b8bfe4ee3a3af0b062
mdrazaulkabir <razaulkabirit@gmail.com>
2026-04-14 23:49:27 +0600

    added the check max number and multiplication

---
+++ week2/given_number_check_max.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int x,max=0;
+    for(int i=1; i<=n; i++){
+        scanf("%d",&x);
+        if(x>=max){
+            max=x;
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
