35ef0a24983246b6c2a893a6039d21eb4cd51330
mdrazaulkabir <razaulkabirit@gmail.com>
2026-04-20 23:48:17 +0600

    max min print only digit previous problem in arry max in min

--- week2/given_number_check_max.c
+++ week2/given_number_check_max.c
@@ -1,14 +1,31 @@
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int x,max=0;
+//     for(int i=1; i<=n; i++){
+//         scanf("%d",&x);
+//         if(x>=max){
+//             max=x;
+//         }
+//     }
+//     printf("%d",max);
+//     return 0;
+// }
+
 #include<stdio.h>
+#include<limits.h>
 int main(){
     int n;
     scanf("%d",&n);
-    int x,max=0;
+    // int x,min;     //eikhane eikar gurbage value astee pare 
+    int x,min=INT_MAX; 
     for(int i=1; i<=n; i++){
         scanf("%d",&x);
-        if(x>=max){
-            max=x;
+        if(x<=min){
+            min=x;
         }
     }
-    printf("%d",max);
+    printf("%d",min);
     return 0;
 }
