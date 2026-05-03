9297744c49e81c9f7580f728017eaa8b1d71191a
mdrazaulkabir <md.razaulkabir.bd@gmail.com>
2026-05-03 23:08:32 +0600

    pattern problem solve

---
+++ important/pattern_problem.c
@@ -0,0 +1,71 @@
+/*
+=>amader fist e dekhte hobe koyta =>line print korte hobe
+=>second koyta =>start print korte hobe 
+=>third koyta space lagbe (zodi lage)
+
+* 
+* * 
+* * * 
+
+*/
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int start=1;
+//     for(int i=1; i<=n; i++){   //this is for the line print 
+//         for(int j=1; j<=start; j++){ 
+//             printf("* ");
+//         }
+//         start++;
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+/*
+
+* * * 
+* * 
+*
+
+*/
+// #include<stdio.h>
+// int main(){
+//     int n; 
+//     scanf("%d",&n);
+//     int strat=n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=strat; j>=1; j--){
+//             printf("* ");
+//         }
+//         strat--;
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int start=1;
+    int spaceStart=n-1;
+    for(int i=1; i<=n; i++){                 ///line print 
+        for(int k=spaceStart; k>=1; k--){    ///space print 
+            printf("  ");
+        }
+        spaceStart--;
+
+        for(int j=1; j<=start; j++){         ///start print
+            printf("* ");
+        }
+        start +=2;
+
+        printf("\n");
+    }
+    return 0;
+}
